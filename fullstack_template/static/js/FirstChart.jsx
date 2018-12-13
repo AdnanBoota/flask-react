cde40bf577626a85f1a1513c136d41ff9174fc53
Adnan Boota <inshallah_1@yahoo.com>
2018-12-13 11:31:51 +0100

    ubuntu updates

--- fullstack_template/static/js/FirstChart.jsx
+++ fullstack_template/static/js/FirstChart.jsx
@@ -38,7 +38,7 @@ export default class FirstChart extends React.Component {
                                 Say Hello!
                             </Button>
 
-                            
+
 
                         </Col>
                     </Row>
